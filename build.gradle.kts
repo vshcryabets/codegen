3c5347e13d4aa21798db880b342dc28ffed6267c
Volodymyr Shkriabets <vshcryabets@gmail.com>
2022-12-28 14:38:15 +0200

    Prepare "fat" jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,23 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "CgenKt"
+    }
+
+//    // To avoid the duplicate handling strategy error
+//    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+//
+//    // To add all of the dependencies
+//    from(sourceSets.main.get().output)
+//
+//    dependsOn(configurations.runtimeClasspath)
+//    from({
+//        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+//    })
+}
+
 application {
     mainClass.set("MainKt")
 }
