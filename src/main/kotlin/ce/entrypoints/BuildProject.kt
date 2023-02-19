d6a6dcc70eb4dbbef9569e0aaa167cfb04313d4c
Volodymyr Shkriabets <vshcryabets@gmail.com>
2023-02-19 21:37:41 +0200

    One entry point for all operations

--- src/main/kotlin/ce/entrypoints/BuildProject.kt
+++ src/main/kotlin/ce/entrypoints/BuildProject.kt
@@ -4,7 +4,11 @@ import ce.defs.*
 import ce.domain.usecase.load.LoadMetaFilesForTargetUseCase
 import ce.domain.usecase.load.LoadProjectUseCase
 import ce.domain.usecase.store.StoreInTreeUseCase
+import ce.domain.usecase.store.StoreOutTreeUseCase
+import ce.domain.usecase.transform.TransformInTreeToOutTreeUseCase
+import ce.repository.GeneratorsRepo
 import ce.settings.Project
+import generators.obj.input.Node
 import java.io.File
 import java.io.FileInputStream
 import java.io.InputStreamReader
@@ -19,12 +23,17 @@ fun main(args: Array<String>) {
     val getProjectUseCase = LoadProjectUseCase()
     val storeInTreeUseCase = StoreInTreeUseCase()
     val loadMetaFilesUseCase = LoadMetaFilesForTargetUseCase()
+    val storeOutTreeUseCase = StoreOutTreeUseCase()
+    val transformInTreeToOutTreeUseCase = TransformInTreeToOutTreeUseCase()
 
     val project : Project = getProjectUseCase(args[0])
     println("Processing $project")
+    val generatorsRepo = GeneratorsRepo(project)
 
     project.targets.forEach { target ->
         val root = loadMetaFilesUseCase(project, target)
         storeInTreeUseCase(project.outputFolder + "input_tree_${target.name}.xml", root)
+        val outTree = transformInTreeToOutTreeUseCase(root, generatorsRepo.get(target))
+        storeOutTreeUseCase(args[2], outTree)
     }
 }
