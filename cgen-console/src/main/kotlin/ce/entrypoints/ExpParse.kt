02bbff18f510a0584fb5b87610872ab3f974f034
Volodymyr Shkriabets <vshcryabets@gmail.com>
2023-04-01 10:48:37 +0300

    Update tokenizer logic

--- cgen-console/src/main/kotlin/ce/entrypoints/ExpParse.kt
+++ cgen-console/src/main/kotlin/ce/entrypoints/ExpParse.kt
@@ -18,6 +18,33 @@ const val spaces = " \n\r\t"
 const val digits = "0123456789"
 const val digitsHex = "0123456789ABCDEF"
 
+
+fun loadDictionary(fileName: String, dictionary: MutableMap<Int, Word>) {
+    val dictionaryFile = File(fileName)
+    if (dictionaryFile.exists()) {
+        CSVReader(FileReader(dictionaryFile)).use { reader ->
+            val r = reader.readAll()
+            r.forEach {
+                dictionary[it[0].toInt()] = Word(it[1])
+            }
+        }
+    }
+}
+
+fun storeDictionary(fileName: String, dictionary: Map<Int, Leaf>) {
+    val dictionaryFile = File(fileName)
+    CSVWriter(FileWriter(dictionaryFile),
+        CSVWriter.DEFAULT_SEPARATOR,
+        CSVWriter.NO_QUOTE_CHARACTER,
+        CSVWriter.NO_ESCAPE_CHARACTER,
+        CSVWriter.DEFAULT_LINE_END).use {
+        dictionary.forEach { id, word ->
+            it.writeNext(arrayOf(id.toString(), "\"${word.name}\""))
+        }
+    }
+}
+
+
 fun readFileLineByLineUsingForEachLine(fileName: String) : StringBuilder {
     val result = StringBuilder()
     File(fileName).forEachLine {
@@ -28,14 +55,22 @@ fun readFileLineByLineUsingForEachLine(fileName: String) : StringBuilder {
 }
 
 data class ParseResult(
-    val wordsMap : Map<Int, Word>
+    val wordsMap : Map<Int, Word> = emptyMap(),
+    val literalsMap : Map<Int, Literal> = emptyMap(),
+    val digits : Map<Int, Digit> = emptyMap(),
+    val tokens: List<Int> = emptyList()
 )
 
 fun buildLinear(buffer: StringBuilder, inPos: Int, dictionary: MutableMap<Int, Word>) : ParseResult {
     var counter = 0
-    val wordsMap = mutableMapOf<Int, Word>().apply {
-        putAll(dictionary)
-    }
+    var literalCounter = 1000000
+    var digitCounter = 2000000
+
+    val literalsMap = mutableMapOf<Int, Literal>()
+    val digitisMap = mutableMapOf<Int, Digit>()
+
+    val numbers = mutableListOf<Int>()
+
     val wordsMapRevers = mutableMapOf<String, Int>().apply {
         putAll(dictionary
             .onEach { if (it.key > counter) counter = it.key }
@@ -44,42 +79,44 @@ fun buildLinear(buffer: StringBuilder, inPos: Int, dictionary: MutableMap<Int, W
         }.toMap())
     }
 
-    val literalsMap = mutableMapOf<Int, Literal>()
-    val digitisMap = mutableMapOf<Int, Digit>()
-    val numbers = mutableListOf<Int>()
     var pos = inPos
     do {
         val ch = buffer.get(pos)
         if (ch == '"') {
             val literalPair = readLiteral(buffer, pos)
-            literalsMap[counter] = literalPair.first
-            numbers.add(counter)
-            counter++
-            println("Literal \"${literalPair.first.name}")
+            literalsMap[literalCounter] = literalPair.first
+            numbers.add(literalCounter)
+            println("Literal \"${literalPair.first.name}\" = $literalCounter")
+            literalCounter++
             pos = literalPair.second
         } else if (ch in spaces) {
             pos++
         } else if (ch in digits) {
             val digit = readDigit(buffer, pos)
+            digitisMap[digitCounter] = digit.first
+            numbers.add(digitCounter)
+            digitCounter++
+            pos = digit.second
         } else {
             val wordPair = readWord(buffer, pos)
             var id = counter
             if (!wordsMapRevers.containsKey(wordPair.first.name)) {
-                wordsMap[id] = wordPair.first
+                dictionary[id] = wordPair.first
                 wordsMapRevers[wordPair.first.name] = id
                 counter++
             } else {
                 id = wordsMapRevers[wordPair.first.name]!!
             }
             numbers.add(id)
-
-            println(wordPair.first.name)
             pos = wordPair.second
         }
     } while (pos < buffer.length)
     println(numbers.toString())
     return ParseResult(
-        wordsMap = wordsMap
+        wordsMap = dictionary,
+        digits = digitisMap,
+        literalsMap = literalsMap,
+        tokens = numbers,
     )
 }
 
@@ -145,27 +182,13 @@ fun main(args: Array<String>) {
     }
 
     val dictionary = mutableMapOf<Int, Word>()
-
-    val dictionaryFile = File("file.csv")
-    if (dictionaryFile.exists()) {
-        CSVReader(FileReader(dictionaryFile)).use { reader ->
-            val r = reader.readAll()
-            r.forEach {
-                dictionary[it[0].toInt()] = Word(it[1])
-            }
-        }
-    }
+    val dictionaryName = "./test/parser/kotlin_dictionary.csv"
+    loadDictionary(dictionaryName, dictionary)
 
     val buffer = readFileLineByLineUsingForEachLine(args[0])
     val result = buildLinear(buffer, 0, dictionary)
 
-    CSVWriter(FileWriter(dictionaryFile),
-        CSVWriter.DEFAULT_SEPARATOR,
-        CSVWriter.NO_QUOTE_CHARACTER,
-        CSVWriter.NO_ESCAPE_CHARACTER,
-        CSVWriter.DEFAULT_LINE_END).use {
-        result.wordsMap.forEach { id, word ->
-            it.writeNext(arrayOf(id.toString(), "\"${word.name}\""))
-        }
-    }
+    storeDictionary(dictionaryName, result.wordsMap)
+    storeDictionary("./test/parser/literals.csv", result.literalsMap)
+    storeDictionary("./test/parser/digits.csv", result.digits)
 }
