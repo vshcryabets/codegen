b6f5ced3ce247f5ac819534edc93eaa671eff64b
Volodymyr Shkriabets <vshcryabets@gmail.com>
2023-06-04 21:52:12 +0300

    Test Gradle Task var insteed val

--- cgen-lib/src/main/kotlin/ce/gradle/GreetingTask.kt
+++ cgen-lib/src/main/kotlin/ce/gradle/GreetingTask.kt
@@ -4,7 +4,7 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
 abstract class GreetingTask : DefaultTask() {
-    abstract val cgenPath : String
+    var cgenPath : String = ""
 
     @TaskAction
     fun greet() {
