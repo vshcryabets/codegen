2486b4e47c7ae0531d12362b0d1ddeb7050a4b6c
Volodymyr Shkriabets <vshcryabets@gmail.com>
2023-06-04 22:01:51 +0300

    Test #3 custom Gradle task

--- cgen-lib/src/main/kotlin/ce/gradle/GreetingTask.kt
+++ cgen-lib/src/main/kotlin/ce/gradle/GreetingTask.kt
@@ -1,13 +1,20 @@
 package ce.gradle
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
 
 abstract class GreetingTask : DefaultTask() {
-    var cgenPath : String = ""
+    @get:Input
+    abstract val cgenPath: Property<String>
+
+    init {
+        cgenPath.convention("hello from GreetingTask")
+    }
 
     @TaskAction
     fun greet() {
-        println("hello from GreetingTask $cgenPath")
+        println("hello from GreetingTask ${cgenPath.get()}")
     }
 }
