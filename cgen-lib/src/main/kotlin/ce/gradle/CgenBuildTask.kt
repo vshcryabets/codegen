20eac1c98420ce807de8cf65e29a222193bc1ab3
Volodymyr Shkriabets <vshcryabets@gmail.com>
2023-06-10 14:56:33 +0300

    Improve CgenBuildTask

---
+++ cgen-lib/src/main/kotlin/ce/gradle/CgenBuildTask.kt
@@ -0,0 +1,31 @@
+package ce.gradle
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.ConfigurableFileCollection
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.TaskAction
+
+abstract class CgenBuildTask : DefaultTask() {
+
+    @get:Input
+    abstract val cgenPath: Property<String>
+    
+    @get:InputFiles
+    abstract val files : ConfigurableFileCollection
+
+    @get:OutputDirectory
+    abstract val outputFolder : DirectoryProperty
+
+    init {
+        cgenPath.convention("hello from CgenBuildTask")
+    }
+
+    @TaskAction
+    fun greet() {
+        println("hello from CgenBuildTask ${cgenPath.get()} $files, $outputFolder")
+    }
+}
