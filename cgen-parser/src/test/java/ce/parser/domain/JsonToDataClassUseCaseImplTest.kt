cbd7752376970ddc3fdb75e3cce919346797e208
V.Shkriabets <vshkriabets@logitech.com>
2023-10-13 19:17:03 +0300

    More tests

--- cgen-parser/src/test/java/ce/parser/domain/JsonToDataClassUseCaseImplTest.kt
+++ cgen-parser/src/test/java/ce/parser/domain/JsonToDataClassUseCaseImplTest.kt
@@ -13,7 +13,26 @@ class JsonToDataClassUseCaseImplTest {
                 {
                     "a": 10,
                     "b": true,
-                    "c": "Test string"
+                    "c": "Test string",
+                    "d": -12.34
+                }
+            """.trimIndent(),
+            "Struct1"
+        )
+        assertEquals(1, res.size)
+        val dataClass = res[0] as DataClass
+        assertEquals(4, dataClass.subs.size)
+    }
+
+    @Test
+    fun testStructWithArrayOfPrimitives() {
+        val useCase = JsonToDataClassUseCaseImpl()
+        val res = useCase(
+            json = """
+                {
+                    "a": 10,
+                    "b": true,
+                    "c": [1,2,3,4,5,6]
                 }
             """.trimIndent(),
             "Struct1"
@@ -24,12 +43,21 @@ class JsonToDataClassUseCaseImplTest {
     }
 
     @Test
-    fun testStructWithArrayOfPrimitives() {
-        assertTrue(false)
-    }
-
-    @Test
     fun testStructWithInnerStruct() {
+        val useCase = JsonToDataClassUseCaseImpl()
+        val res = useCase(
+            json = """
+                {
+                    "a": 10,
+                    "b": true,
+                    "cStruct": {"s1": true, "s2": 25}
+                }
+            """.trimIndent(),
+            "Struct1"
+        )
+        assertEquals(1, res.size)
+        val dataClass = res[0] as DataClass
+        assertEquals(4, dataClass.subs.size)
         assertTrue(false)
     }
 
