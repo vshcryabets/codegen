50cd4654be52ebba26886ce36618bfd1fa773cc1
V.Shkriabets <vshcryabets@gmail.com>
2025-08-24 17:59:34 +0300

    Small fixes

--- cgen-lib/src/main/kotlin/ce/defs/TargetExt.kt
+++ cgen-lib/src/main/kotlin/ce/defs/TargetExt.kt
@@ -2,5 +2,5 @@ package ce.defs
 
 object TargetExt {
     fun findByName(name: String): Target =
-        Target.values().find {it.name.equals(name, true) } ?: Target.Other
+        Target.entries.find {it.name.equals(name, true) } ?: Target.Other
 }
