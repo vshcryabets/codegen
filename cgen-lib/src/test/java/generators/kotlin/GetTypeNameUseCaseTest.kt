c37073fbfd5ebd2688569df7e85df92c77d8f843
Volodymyr Shkriabets <vshkriabets@logitech.com>
2026-04-07 08:35:28 +0300

    Fix nullable strings in kotlin (#50)
    * Fix nullable strings in kotlin
    
    * Fix comments

---
+++ cgen-lib/src/test/java/generators/kotlin/GetTypeNameUseCaseTest.kt
@@ -0,0 +1,22 @@
+package generators.kotlin
+
+import ce.defs.DataType
+import generators.obj.syntaxParseTree.FileDataImpl
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class GetTypeNameUseCaseTest {
+    private val getArrayDataTypeUseCase = GetArrayDataTypeUseCase()
+    private val useCase = GetTypeNameUseCase(getArrayDataTypeUseCase)
+
+    @Test
+    fun testStringNullable() {
+        val fileData = FileDataImpl(
+            name = "testFile",
+            subs = mutableListOf(),
+            isDirty = true
+        )
+        val result = useCase.typeTo(fileData, DataType.stringNullable)
+        Assertions.assertEquals("String?", result)
+    }
+}
